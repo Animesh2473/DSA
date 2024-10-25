275a777fb61ebdf884b2c04d4e8e46d2a4d784ee
Animesh Vijay Kewale <139259324+Animesh2473@users.noreply.github.com>
2024-10-25 19:03:50 +0530

    c

---
+++ CPP/Level 1/25.Pointers in C++/1_intro.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ 
+ int num = 5;
+
+ cout<<"num is "<<num<<endl;
+
+ // adress of Oprater = &
+
+ cout<<"Adress of num is "<<&num<<endl;
+ 
+ 
+return 0;
+}
