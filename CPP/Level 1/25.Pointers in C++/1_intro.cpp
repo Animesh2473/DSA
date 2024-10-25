5a3c8f149ac89e3f5bbd003eed8fa2912a277288
Animesh Vijay Kewale <139259324+Animesh2473@users.noreply.github.com>
2024-10-25 19:45:37 +0530

    pointers1

--- CPP/Level 1/25.Pointers in C++/1_intro.cpp
+++ CPP/Level 1/25.Pointers in C++/1_intro.cpp
@@ -14,8 +14,20 @@ int main()
 int *ptr = &num;
 
 
- cout<<"Value of *ptr is "<<num<<endl;
- 
- 
+ cout<<"Value of *ptr is "<<*ptr<<endl;
+
+ cout<<"Value of ptr is "<<ptr<<endl;
+double d = 4.56;
+double *p2 = &d;
+
+
+ cout<<"Adress of d is "<<p2<<endl;
+ cout<<"Value of d is "<<*p2<<endl;
+
+
+
+ cout<<"size of integer is "<<sizeof(num)<<endl;
+ cout<<"size of pointer is "<<sizeof(ptr)<<endl;
+ cout<<"size of pointer 2 is "<<sizeof(p2)<<endl;
 return 0;
 }
