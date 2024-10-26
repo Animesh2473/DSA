7627d97dff20be1d551da1a242d27257814f7699
Animesh Vijay Kewale <139259324+Animesh2473@users.noreply.github.com>
2024-10-26 07:25:55 +0530

    c

---
+++ CPP/Level 1/25.Pointers in C++/2_play.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  
+
+//pointer to int is created ,and pointing towards some  garbage "adress"
+
+int *p=0;
+
+cout<<*p<<endl;
+ 
+return 0;
+}
