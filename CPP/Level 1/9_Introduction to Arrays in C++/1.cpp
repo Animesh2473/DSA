65e1b05822fdef601aa7733ca9dea2c1d21b1cf2
Animesh Vijay Kewale <139259324+Animesh2473@users.noreply.github.com>
2024-10-26 20:46:26 +0530

    commit 3

--- CPP/Level 1/9_Introduction to Arrays in C++/1.cpp
+++ CPP/Level 1/9_Introduction to Arrays in C++/1.cpp
@@ -2,8 +2,13 @@
 using namespace std;
 int main()
 {
- 
- 
+
+// declare an array
+
+int number[15];
+
+cout<<"Value at 3 index"<<number[3]<<endl;
+cout<<endl<<"Everything is Fine"<<endl<<endl;
  
  
 return 0;
