f88e82c490bf97c5d2f80e454a4267e5cb878537
Animesh Vijay Kewale <139259324+Animesh2473@users.noreply.github.com>
2024-10-26 11:56:00 +0530

    commit

---
+++ CPP/Level 1/25.Pointers in C++/Daughts/lecture25.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int i = 5;
+
+
+// Acess i with pointer Method 1
+int *p = &i;
+cout<<"*p : "<<*p<<endl; 
+cout<<"p : "<<p<<endl; 
+
+//Acress i with pointer Method 2
+
+int *q = 0;//optional you just have to creat a pointer
+q = &i;
+cout<<"*q : "<<*q<<endl; 
+cout<<"q : "<<q<<endl; 
+
+//experiment 1
+
+int *w;
+w = &i;
+cout<<"*w: "<<*w<<endl; 
+cout<<"w: "<<w<<endl; 
+
+//copy pointer
+int *s=p;//this is copy pointer
+
+
+cout<<"Copy pointer s value: "<<*s<<endl;
+cout<<"Copy pointer s address: "<<s<<endl;
+
+//Arithmatic oprations with pointers
+//Method 1(A)
+(*p)++;
+cout<<"*q : "<<*q<<endl; 
+//Method 1(B)
+cout<<"*q : "<< (*p)++ <<endl; 
+
+//Method 2
+*p = *p + 1;
+cout<<"*p : "<<*p<<endl; //Daught
+
+//
+
+ 
+return 0;
+}
