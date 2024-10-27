639162fdf1aecd5c58dde18debb04e59fe6636c2
Animesh Vijay Kewale <139259324+Animesh2473@users.noreply.github.com>
2024-10-27 11:01:51 +0530

    commmit 1

--- CPP/Level 1/9_Introduction to Arrays in C++/1.cpp
+++ CPP/Level 1/9_Introduction to Arrays in C++/1.cpp
@@ -1,15 +1,59 @@
 #include<iostream>
 using namespace std;
+
+//Hold
+void printArray(){
+
+}
+
 int main()
 {
 
-// declare an array
+//declaration of an array
+
+cout<<"Array 1"<<endl;
 
 int number[15];
 
-cout<<"Value at 3 index"<<number[3]<<endl;
-cout<<endl<<"Everything is Fine"<<endl<<endl;
+cout<<"Value at 1 index is "<<number[0]<<endl;
+cout<<"Value at 2 index is "<<number[1]<<endl;
+cout<<"Value at 15 index is "<<number[14]<<endl;
+
+//Acesing more than an index of an array
+cout<<"Value at 16 index is "<<number[15]<<endl;
+cout<<"Value at 19 index is "<<number[20]<<endl;
+cout<<"Value at 20 index is "<<number[21]<<endl;
+
  
- 
+cout<<"Array 2"<<endl;
+
+int secand[3]={10,11,12};
+
+cout<<"Value at 1 index is "<<secand[0]<<endl;
+cout<<"Value at 2 index is "<<secand[1]<<endl;
+cout<<"Value at 3 index is "<<secand[2]<<endl;
+
+
+cout<<"Array 3"<<endl;
+
+int third[15] = {2,7};
+cout<<"Value at 1 index is "<<third[0]<<endl;
+cout<<"Value at 2 index is "<<third[1]<<endl;
+
+
+cout<<""<<endl;
+
+cout<<"Loop starts"<<endl;
+
+int n=20;
+
+for (int i = 0; i < n; i++)
+{
+    
+cout<<"Value at "<<i +1<<" index is "<<third[i]<<endl;
+}
+
+
+cout<<"Everything is fine "<<endl;
 return 0;
 }
