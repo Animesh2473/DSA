b7a77fc3d0ceb5890ac6da558a4e99b1ec1af357
Animesh Vijay Kewale <139259324+Animesh2473@users.noreply.github.com>
2024-12-13 07:47:32 +0530

    commit 1

---
+++ Lecture 2 Write Your First Program in C++/2_DataTypes.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ 
+ int a = 'a';
+ cout<<a<<endl;
+
+ char ch = 98;
+
+ cout<<ch<<endl;
+
+ //This is known as type-casting
+ 
+char ch1 = 123456;
+cout<<ch1<<endl;
+
+ 
+return 0;
+}
