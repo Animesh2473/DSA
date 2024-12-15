b998a675d87d3d39f5abb2d5b4927b9b800f51d2
Animesh Vijay Kewale <139259324+Animesh2473@users.noreply.github.com>
2024-12-15 15:14:02 +0530

    c

---
+++ Lecture 3 If-Else, While loop & Lots of Patterns (Part-1)/1_questions.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+// int a,b;
+
+// cout<<"Enter the value of a"<<endl;
+// cin>>a;
+
+
+// cout<<"Enter the value of b"<<endl;
+// cin>>b;
+
+// if (a>b)
+// {
+//     cout<< "A is greater" << endl;
+
+// }
+
+// if (b>a)
+// {
+//     cout<< "B is greater" << endl;
+// }
+
+int a;
+
+cout<<"Enter the value of a"<<endl;
+cin>>a;
+
+if (a>0)
+{
+    cout<<"A is Positive"<<endl;
+
+}
+
+if (a<0)
+{
+    cout<<"A is negative"<<endl;
+}
+
+else{
+
+    cout<<"A is 0";
+}
+
+
+ 
+return 0;
+}
